69b75787236a3f769f97ffd4c7b6f220c159eeae
nar17 <122194228+nar17@users.noreply.github.com>
2023-01-11 22:22:02 -0600

    pt F visualizing, multiple sensors, prettifying

--- analyze.py
+++ analyze.py
@@ -1,3 +1,10 @@
 import numpy
+import matplotlib.pyplot
 
-numpy.load(data/backLegSensorValues.npy)
+backLegSensorValues = numpy.load("data/backLegSensorValuesData.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValuesData.npy")
+
+matplotlib.pyplot.plot(backLegSensorValues, linewidth=3, label='Back Leg')
+matplotlib.pyplot.plot(frontLegSensorValues, linewidth=2, label='Front Leg')
+matplotlib.pyplot.legend(loc=0, shadow=True)
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -16,16 +16,18 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
 for i in range (1000):
 	p.stepSimulation()
 	time.sleep(1/60)
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg")
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg")
+	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg")
 
 numpy.save(os.path.join('data', 'backLegSensorValuesData'), backLegSensorValues)
+numpy.save(os.path.join('data', 'frontLegSensorValuesData'), frontLegSensorValues)
 
 p.disconnect()
 
 print(backLegSensorValues)
-
+print(frontLegSensorValues)
