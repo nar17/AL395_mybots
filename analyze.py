6aff1e150ed6697472316e7e7e4e1587eb23ef8b
nar17 <122194228+nar17@users.noreply.github.com>
2023-01-11 22:26:26 -0600

    pt F dashed back leg

--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot
 backLegSensorValues = numpy.load("data/backLegSensorValuesData.npy")
 frontLegSensorValues = numpy.load("data/frontLegSensorValuesData.npy")
 
-matplotlib.pyplot.plot(backLegSensorValues, linewidth=3, label='Back Leg')
+matplotlib.pyplot.plot(backLegSensorValues, linewidth=3, linestyle='dashed', label='Back Leg')
 matplotlib.pyplot.plot(frontLegSensorValues, linewidth=2, label='Front Leg')
 matplotlib.pyplot.legend(loc=0, shadow=True)
 matplotlib.pyplot.show()
