293a9b285c714b090249ed2f6cfc5911dc91745d
nar17 <122194228+nar17@users.noreply.github.com>
2023-01-15 00:04:55 -0600

    pt H struggling at step 64

--- robot.py
+++ robot.py
@@ -1,11 +1,26 @@
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
+import numpy
+
+from sensor import SENSOR
 
 class ROBOT:
 
 	def __init__(self):
-		self.sensors = {}
 		self.motors = {}
 		self.robotId = p.loadURDF("body.urdf")
 		pyrosim.Prepare_To_Simulate(self.robotId)
-		
+		self.Prepare_To_Sense()
+	
+	def Prepare_To_Sense(self):
+		self.sensors = {}
+		for linkName in pyrosim.linkNamesToIndices:
+			self.sensors[linkName] = SENSOR(linkName)
+
+	def Sense(self, t):
+		self.linkName = linkName
+		for i in self.sensors:
+			self.sensors[linkName]
+
+
+
--- sensor.py
+++ sensor.py
@@ -1,3 +1,11 @@
+import pyrosim.pyrosim as pyrosim
+import numpy
+
 class SENSOR:
-	def __init__(self):
-		pass
+	def __init__(self, linkName):
+		self.linkName = linkName
+		self.values = numpy.zeros(1000)
+
+	def Get_Value(self):
+		self.values = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
+	
