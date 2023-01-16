23ce7917294bc8392901114f67298a22d6de3c8c
nar17 <122194228+nar17@users.noreply.github.com>
2023-01-16 17:59:16 -0600

    pt i sense think act

--- robot.py
+++ robot.py
@@ -4,6 +4,7 @@ import numpy
 
 from sensor import SENSOR
 from motor import MOTOR
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
 
@@ -14,6 +15,7 @@ class ROBOT:
 		pyrosim.Prepare_To_Simulate(self.robotId)
 		self.Prepare_To_Sense()
 		self.Prepare_To_Act()
+		self.nn = NEURAL_NETWORK("brain.nndf")
 	
 	def Prepare_To_Sense(self):
 		for linkName in pyrosim.linkNamesToIndices:
@@ -29,6 +31,10 @@ class ROBOT:
 
 	def Act(self, t):
 		for i in self.motors:
-			self.motors[i].Set_Value(t, self.robotId)	
+			self.motors[i].Set_Value(t, self.robotId)
+
+	def Think(self):
+		self.nn.Print()
+
 
 
